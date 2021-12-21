f39c001d1bcfb92bb2a6871f29b294f3b8b71c8d
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2021-12-21 12:53:02 +0300

    near define and get its value

--- src/simple/__tests__/index.unit.spec.ts
+++ src/simple/__tests__/index.unit.spec.ts
@@ -3,17 +3,9 @@ import * as contract from "../assembly";
 describe("Contract", () => {
   // VIEW method tests
 
-  it("says hello", () => {
-    expect(contract.helloWorld()).toStrictEqual("hello world")
+  it("gives the value of one near", () => {
+    expect(contract.getNear()).toStrictEqual(contract.ONE_NEAR)
   })
 
-  it("reads data", () => {
-    expect(contract.read("some key")).toStrictEqual("🚫 Key [ some key ] not found in storage. ( storage [ 0 bytes ] )")
-  })
-
-  // CHANGE method tests
-
-  it("saves data to contract storage", () => {
-    expect(contract.write("some-key", "some value")).toStrictEqual("✅ Data saved. ( storage [ 18 bytes ] )")
-  })
+  
 })
--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,26 +1,7 @@
-import { storage, Context } from "near-sdk-as"
+import { u128 } from "near-sdk-as"
 
-// return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
-}
-
-// read the given key from account (contract) storage
-export function read(key: string): string {
-  if (storage.hasKey(key)) {
-    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
-  } else {
-    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`
-  }
-}
-
-// write the given value at the given key to account (contract) storage
-export function write(key: string, value: string): string {
-  storage.set(key, value)
-  return `✅ Data saved. ( ${storageReport()} )`
-}
-
-// private helper method used by read() and write() above
-function storageReport(): string {
-  return `storage [ ${Context.storageUsage} bytes ]`
+// when exporting the variable we can use it in another files 
+export const ONE_NEAR =  u128.from("1000000000000000000000000");
+export function getNear():u128{
+  return ONE_NEAR
 }
