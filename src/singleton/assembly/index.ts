f2fdc78ac0505c0408d6baa6ed590ceb4f43e882
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2021-12-21 18:15:13 +0300

    conditionat statement

--- src/singleton/assembly/index.ts
+++ src/singleton/assembly/index.ts
@@ -1,53 +1,22 @@
-import { storage, Context } from "near-sdk-core"
-
 @nearBindgen
 export class Contract {
-  private message: string = 'hello world'
+  myValue:i32 = 10;
 
-  // return the string 'hello world'
-  helloWorld(): string {
-    return this.message
-  }
-
-  // read the given key from account (contract) storage
-  read(key: string): string {
-    if (isKeyInStorage(key)) {
-      return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
-    } else {
-      return `🚫 Key [ ${key} ] not found in storage. ( ${this.storageReport()} )`
+  isGreater(val:i32):bool {
+    if(val > this.myValue){
+      return true
+    }else{
+      return false;
     }
   }
 
-  // write the given value at the given key to account (contract) storage
-  @mutateState()
-  write(key: string, value: string): string {
-    storage.set(key, value)
-    return `✅ Data saved. ( ${this.storageReport()} )`
+  // Ternary if-else
+  // this example exists in https://www.assemblyscript.org/examples/snippets.html#switch-case
+  isTrue(a: i32): i32 {
+    let response = a > 0 ? 1 : 0;
+    return response;
   }
 
-  // private helper method used by read() and write() above
-  private storageReport(): string {
-    return `storage [ ${Context.storageUsage} bytes ]`
-  }
+  
 }
 
-/**
- * This function exists only to avoid a compiler error
- *
-
-ERROR TS2339: Property 'contains' does not exist on type 'src/singleton/assembly/index/Contract'.
-
-     return this.contains(key);
-                 ~~~~~~~~
- in ~lib/near-sdk-core/storage.ts(119,17)
-
-/Users/sherif/Documents/code/near/_projects/edu.t3/starter--near-sdk-as/node_modules/asbuild/dist/main.js:6
-        throw err;
-        ^
-
- * @param key string key in account storage
- * @returns boolean indicating whether key exists
- */
-function isKeyInStorage(key: string): bool {
-  return storage.hasKey(key)
-}
