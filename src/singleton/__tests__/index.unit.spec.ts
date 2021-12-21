48d24193fb1e453e7f9c5d1fae529540720e28f0
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2021-12-21 11:01:35 +0300

    helloWorld class

--- src/singleton/__tests__/index.unit.spec.ts
+++ src/singleton/__tests__/index.unit.spec.ts
@@ -12,14 +12,5 @@ describe("Contract", () => {
   it("says hello", () => {
     expect(contract.helloWorld()).toStrictEqual("hello world")
   })
-
-  it("reads data", () => {
-    expect(contract.read("some key")).toStrictEqual("🚫 Key [ some key ] not found in storage. ( storage [ 0 bytes ] )")
-  })
-
-  // CHANGE method tests
-
-  it("saves data to contract storage", () => {
-    expect(contract.write("some-key", "some value")).toStrictEqual("✅ Data saved. ( storage [ 18 bytes ] )")
-  })
+  
 })
